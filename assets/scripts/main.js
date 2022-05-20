3e3943bde870ae96689b51856a3f4d4dee69e1d7
emre <emre@emre-MacBook-Air.local>
2022-05-20 11:49:52 +0300

    updates

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -129,3 +129,18 @@ if (t) {
     };
 }
 /* END: index.html GÜL efekti */
+
+
+/* services filter düğmelerinin active durumlarını kontrol etme */
+// Get the container element
+var btnContainer = document.querySelector(".services-filter-btns-wrapper");
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("filter-btn");
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
